014f778d03e01fdb417095cf98d3be90417a55dc
6353079417 <dhavalchauhan00321@gmail.com>
2024-06-04 18:39:29 +0530

    add file

---
+++ module 1.mjs
@@ -0,0 +1,6 @@
+import text,{a,c,d} from './module 2.mjs';
+
+console.log(text);
+console.log(a);
+console.log(c);
+console.log(d);
---
+++ module 2.mjs
@@ -0,0 +1,10 @@
+const a = "HT";
+const b = "What is Your Name ?";
+const c = "How Are You ?";
+const d = "I am Fine";
+
+
+export default b;
+export {a};
+export {c};
+export {d};
