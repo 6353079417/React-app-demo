a1974e2899190216bdc500212e8bcd925dbfa6bb
6353079417 <dhavalchauhan00321@gmail.com>
2024-06-08 17:11:05 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import {useState} from 'react' 
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 
 function App() {
+    const [mode,setMode] = useState('light')
+
+    const toggleMode = () => {
+      if (mode === 'light') {
+        setMode('dark');
+        document.body.style.background="black"
+      } else {
+        setMode("light");
+        document.body.style.background="white"
+      }
+    }
   return (
     <>
-      <Navbar title="SIT" aboutText="Contact Us"/>
+      <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode = {toggleMode} />
       <TextForm heading="Enter Text to Analyse Below"/>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark "  data-bs-theme="dark" >
+            <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
                
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">{props.title}</a>
@@ -26,9 +26,10 @@ export default function Navbar(props) {
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            
-                            <button className="btn btn-outline-warning" type="submit">Search</button>
+                        <div className="form-check form-switch">
+                          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>{props.mode}
+                          
+                        </div>
                         </form>
                     </div>
                 </div>
