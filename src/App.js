107976b4863924984e304d730fe0be42336fb9bb
6353079417 <dhavalchauhan00321@gmail.com>
2024-06-13 10:54:52 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 
 function App() {
@@ -9,24 +10,39 @@ function App() {
 
     const [btnText,setBtnText] = useState('Enable Dark Mode')
 
+    const [alert,setAlert] = useState(null);
+
+    const showAlert = (message,type) => {
+      setAlert({
+        msg:message,
+        type:type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      },1500);
+    }
+
     const toggleMode = () => {
       if (mode === 'light') {
         setMode('dark');
         document.body.style.background="black"
         document.body.style.color="white"
-        setBtnText("Enable light Mode")        
+        setBtnText("Enable light Mode")  
+        showAlert("Dark mode has been Enable","success")  ;    
       } else {
-        setMode("light")
-        document.body.style.background="white"
-        document.body.style.color="black"
-        setBtnText("Enable Dark Mode")
+        setMode("light");
+        document.body.style.background="white";
+        document.body.style.color="black";
+        setBtnText("Enable Dark Mode");
+        showAlert("light Mode Has Been Enable","success");
       }
     }
     
   return (
     <>
       <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode = {toggleMode} btnText={btnText}/>
-      <TextForm toggleMode = {toggleMode} mode = {mode} heading="Enter Text to Analyse Below"/>
+      <Alert alert = {alert}/>
+      <TextForm toggleMode = {toggleMode} mode = {mode} heading="Enter Text to Analyse Below" showAlert={showAlert}/>
     </>
   );
 }
